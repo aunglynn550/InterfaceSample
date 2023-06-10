4875720535144085729445a04505220548331880
aunglynn550 <aunglynn550@gmail.com>
2023-06-10 09:42:59 +0630

    InterfaceExample

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'Application.php';
+require_once 'MysqlDb.php';
+require_once 'SqliteDb.php';
+require_once 'RESTDb.php';
+
+$app = new Application(
+   // new MysqlDb()
+    new RESTDb()
+   // new SqliteDb()
+);
+
+$app->db->getOrders();
+$app->db->getOrderById(1);
+$app->db->createOrder([]);
+$app->db->updateOrder(1,[]);
+$app->db->deleteOrder(1);
